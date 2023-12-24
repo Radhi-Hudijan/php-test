4c165fbf1f23e9f981917711e6b0c0be0114eaab
Radhi Hudijan <radhif2009@gmail.com>
2023-12-24 06:48:59 +0100

    output methods

--- 01_output.php
+++ 01_output.php
@@ -1,3 +1,15 @@
 <?php 
-echo 'Radhi134'
+
+//echo
+//echo 'Radhi123',555,10.5;
+
+//print : only single value
+//print 'Ali';
+
+// print_r() - function to print values and arrays
+// print_r([1,2,3])
+
+// var_dump('hello');
+
+var_export('hello')
 ?>
