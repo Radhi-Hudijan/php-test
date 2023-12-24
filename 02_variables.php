7f9a93afe1ae49ec83df6636d0755b99c39e87b8
Radhi Hudijan <radhif2009@gmail.com>
2023-12-24 08:17:47 +0100

    variables

---
+++ 02_variables.php
@@ -0,0 +1,22 @@
+<?php 
+// string
+$name = 'Brad'; //string
+$age = 40;  //Int
+
+$has_kids = true;
+
+$cash_on_hand = 20.5 ; // Float
+
+//var_dump($has_kids);
+
+//echo $name . "is" . $age;
+
+//echo " $name is $age"
+
+$x = '5' + '5' ;
+
+define('HOST','localhost') // constant
+
+
+
+?>
