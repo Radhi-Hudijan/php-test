ad2bfc556df11a6df30469e3f5250aa787a0d156
Radhi Hudijan <radhif2009@gmail.com>
2023-12-25 08:45:00 +0100

    functions

---
+++ 06_functions.php
@@ -0,0 +1,38 @@
+<?php
+
+// $y = 12;
+// function FunctionName()
+// {
+//     global $y;
+//     echo $y;
+//     echo "welcome";
+// }
+
+// FunctionName();
+
+
+// function FunctionName($email)
+// {
+//     echo $email . ' Registered';
+// }
+
+// FunctionName('Radhi');
+
+// function sum($n1  = 4, $n2 = 5)
+// {
+//     return ($n1 + $n2);
+// }
+
+// $total = sum();
+
+// echo $total;
+
+$subtract = function ($n1, $n2) {
+    return $n1 - $n2;
+};
+
+// echo $subtract(4, 2);
+
+// $multiply = fn ($n3, $n4) => $n3 * $n4;
+
+// $multiply = fn ($n3, $n4) => $n3 * $n4;
